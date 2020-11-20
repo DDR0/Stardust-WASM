471251d315205042bfa7455fde613a0d99184969
DDR <d@ddr0.ca>
2020-11-20 13:27:30 -0800

    Further hard-won improvements to clean.sh. node_modules must be cleared too.

--- clean.sh
+++ clean.sh
@@ -1,8 +1,12 @@
-rm -r crate-wasm/pkg target/*
+#!/bin/bash
+#Try to get rid of all the state that keeps us from compiling when bumping versions.
+#Deleting each one of these has, at some point, been the key to getting things working again.
+
+rm -rf Cargo.lock crate-wasm/pkg target/*
 cargo build &
 
 cd www
-	rm package-lock.json
+	rm -rf package-lock.json node_modules
 	npm install &
 cd ../
 

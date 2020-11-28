12cbc9e8cf5861f604da02d4b097859bd84e104f
DDR <d@ddr0.ca>
2020-11-27 16:43:20 -0800

    Made it so you can drag around nodes and the camera.

--- crate-wasm/src/lib.rs
+++ crate-wasm/src/lib.rs
@@ -8,12 +8,12 @@ mod utils;
 
 //Limits of array data coming from JS. Mirrored in graphData.mjs.
 const MAX_NODES: usize = 1<<16;
-const MAX_LINKS: usize = 1<<16;
-const LINKS_PER_NODE: usize = 32;
+//const MAX_LINKS: usize = 1<<16;
+//const LINKS_PER_NODE: usize = 32;
 
 
 const IDEAL_NODE_DISTANCE: f32 = 80.0; //svg css px, effectively arbitrary.
-const SLOWDOWN_FACTOR: f32 = 40.0;
+const SLOWDOWN_FACTOR: f32 = 30.0;
 const PADDING: f32 = 0.1; //svg css px
 
 
@@ -26,21 +26,21 @@ pub fn optimize_graph(
 	x: Float32Array,
 	y: Float32Array,
 	flags: Uint8Array,
-	links: Uint16Array,
-	num_links: Uint8Array,
+	_links: Uint16Array,
+	_num_links: Uint8Array,
 	node_count: Uint16Array,
 
 	//Links
 	from: Uint16Array,
 	to: Uint16Array,
-	lflags: Uint8Array,
+	_lflags: Uint8Array,
 	link_count: Uint16Array,
 ) {
 	//console::log_1(&"optimize".into())
 	//Two-sweep system - first, sum the forces on each node.
 	//This could really stand to be accelerated by a b-tree or something, but since we've put it in a web worker we can just ... eat the cost for now.
 	
-	#[derive(Debug, Clone, Copy)]
+	#[derive(Clone, Copy)]
 	struct NodeForces {
 		x: f32, //vector magnitudes
 		y: f32,
@@ -54,10 +54,10 @@ pub fn optimize_graph(
 			let i_ = i as usize;
 			NODE_FORCES[i_].x = 0.0; //Unweight the force vector and zero it so we can just iterate through in the set loop.
 			NODE_FORCES[i_].y = 0.0;
-			if flags.get_index(i)&1 == 0 { continue } //Skip dead source node.
+			if flags.get_index(i)&0b11 != 0b01 { continue } //Skip moving dead or frozen source node.
 			for j in 0_u32..node_count.get_index(0) as u32 {
 				if i == j { continue } //skip self
-				if flags.get_index(j)&1 == 0 { continue } //Skip dead dest node.
+				if flags.get_index(j)&0b1 != 0b1 { continue } //Skip dead nodes.
 				if (x.get_index(i) - x.get_index(j)).abs() + PADDING >= IDEAL_NODE_DISTANCE //Cheap square test. π/4=78% accurate!
 				|| (y.get_index(i) - y.get_index(j)).abs() + PADDING >= IDEAL_NODE_DISTANCE { continue } //Skip distant nodes.
 				
@@ -86,23 +86,23 @@ pub fn optimize_graph(
 			if len - PADDING <= IDEAL_NODE_DISTANCE { continue }
 			
 			//TODO: Weight movement that would snap to the ideal distance heavier than other weight.
-			NODE_FORCES[i_].x += len_x/len * (len-IDEAL_NODE_DISTANCE);
-			NODE_FORCES[i_].y += len_y/len * (len-IDEAL_NODE_DISTANCE);
+			if flags.get_index(i)&0b11 == 0b01 { //Skip dead or frozen source node.
+				NODE_FORCES[i_].x += len_x/len * (len-IDEAL_NODE_DISTANCE);
+				NODE_FORCES[i_].y += len_y/len * (len-IDEAL_NODE_DISTANCE);
+			}
 			
-			NODE_FORCES[j_].x -= len_x/len * (len-IDEAL_NODE_DISTANCE);
-			NODE_FORCES[j_].y -= len_y/len * (len-IDEAL_NODE_DISTANCE);
+			if flags.get_index(j)&0b11 == 0b01 {
+				NODE_FORCES[j_].x -= len_x/len * (len-IDEAL_NODE_DISTANCE);
+				NODE_FORCES[j_].y -= len_y/len * (len-IDEAL_NODE_DISTANCE);
+			}
 		}
 		
 		//3. for each node, move by NODE_FORCES
+		//Note that if the node has moved while were were calculating this step, the motion will be retained but the velocity will be incorrect for one frame.
 		for i in 0..node_count.get_index(0) as u32 {
-			x.set_index(i, x.get_index(i) - (NODE_FORCES[i as usize].x / SLOWDOWN_FACTOR));
-			y.set_index(i, y.get_index(i) - (NODE_FORCES[i as usize].y / SLOWDOWN_FACTOR));
+			x.set_index(i, x.get_index(i) - (NODE_FORCES[i as usize].x / (SLOWDOWN_FACTOR/2.0) ));
+			y.set_index(i, y.get_index(i) - (NODE_FORCES[i as usize].y / (SLOWDOWN_FACTOR/2.0) ));
 		}
-			
-	
-	    // console::log_1(&format!("optimized graph at addr: {:p}", &NODE_FORCES).into());
-	    NODE_FORCES[0].x = -50.0;
-	    x.set_index(0, NODE_FORCES[0].x);
 	}
 }
 
--- www/app/graphUi.mjs
+++ www/app/graphUi.mjs
@@ -5,6 +5,11 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 
+
+///////////////////////
+//  Graph Rendering  //
+///////////////////////
+
 const updateNodeType = [
 	(data, elem) => { //circle
 		const circle = elem.querySelector('circle')
@@ -59,6 +64,14 @@ export const link = newGraphData => {
 	linkTemplates = $$("#graph-link-templates > g")
 	nodeTemplates = $$("#graph-node-templates > g")
 	repaint()
+	
+	const svg = $('#graph > svg')
+	svg.addEventListener('mousedown',  svgActivated)
+	svg.addEventListener('mousemove',  svgMouseMove)
+	svg.addEventListener('mouseup',    svgDeactivated)
+	svg.addEventListener('mouseleave', svgDeactivated)
+	svg.addEventListener('wheel',      svgWheel)
+	svg.currentScale = 1.5
 }
 
 let paintPending = false
@@ -162,14 +175,103 @@ const render = ()=>{
 			cached.visible = visible
 		}
 		
+		//If needed, transition shape.
 		const shape = data.flags>>3&3
 		if (shape !== cached.lastShape) {
 			cached.lastShape = shape
 			const newNode = nodeTemplates[shape].cloneNode(true)
 			$('#graph-layer-nodes').replaceChild(newNode, cached.domNode)
 			cached.domNode = newNode
+			
+			//Add event listeners to first child, the background of the node type, because it's the consistent thing for mouseup/mouseleave to report as their evt.target.
+			newNode.addEventListener('mousedown',  nodeActivated.bind(0, i))
 		}
 		
 		updateNodeType[shape](data, cached.domNode)
 	}
+}
+
+
+
+
+/////////////////////////
+//  Graph Interaction  //
+/////////////////////////
+
+//Previously in graph interaction, we had attached event handlers to the SVG
+//elements themselves to handle dragging. However, this caused some issues
+//with the mouse moving off the element when dragging, and thus stopping the
+//drag. Instead, what we do now is register an active element, and if there is
+//one drag that around instead of the canvas.
+
+let activeObj = null // { node: <g>, nodeIndex: int, nodeShape: int } //node must be synced with nodeShape, so keep it all together
+
+//Start dragging a node.
+const nodeActivated = (nodeIndex, evt) => {
+	evt.target.closest('svg').setAttribute('dragging', '') //Disable further mouse events on svg elements.
+	graphData.nodes.flags[nodeIndex] |= 2 //Freeze the current object, so graph optimization doesn't move it around.
+	activeObj = {
+		node: evt.target.closest('.graph-node'), //Our target is the graph node *group*, but that can't be clicked on so we have to find it from its child.
+		nodeIndex,
+		nodeShape: graphData.nodes.flags[nodeIndex]>>3&3,
+	}
+}
+
+//Start panning the camera.
+const svgActivated = evt => {
+	if (evt.target.tagName !== 'svg') { return }
+	evt.target.setAttribute('dragging', '')
+}
+
+//Move something.
+const svgMouseMove = evt => {
+	if (!evt.target.hasAttribute('dragging')) { return }
+	//Note: .movementX/Y is a CR as of 2020-11-27, but seems widely supported and is *just* the ticket here.
+	if (activeObj) { //Move the active graph node.
+		graphData.nodes.x[activeObj.nodeIndex] += evt.movementX / evt.target.currentScale / devicePixelRatio
+		graphData.nodes.y[activeObj.nodeIndex] += evt.movementY / evt.target.currentScale / devicePixelRatio
+		updateNodeType[activeObj.nodeShape](graphData.node(activeObj.nodeIndex), activeObj.node)
+	} else { //Move the camera.
+		const target = evt.target.closest('svg')
+		target.currentTranslate.x += evt.movementX
+		target.currentTranslate.y += evt.movementY
+	}
+	
+}
+
+//Stop dragging and release active node if any.
+const svgDeactivated = evt => {
+	//$('#graph > svg').removeAttribute('dragging')
+	evt.target.removeAttribute('dragging')
+	
+	if (activeObj) {
+		graphData.nodes.flags[activeObj.nodeIndex] &= ~2 //Clear object freeze.
+		activeObj = null
+	}
+}
+
+//svg.addEventListener('wheel', svgWheel)
+const svgWheel = evt => {
+	const target = evt.target.closest('svg')
+	
+	//First, calcualte the new scale we want.
+	const deltaYInPx = evt.deltaY * Math.pow(evt.deltaMode+1, 5) //Convert page and line scroll modes into something ~appropriate. Hopefully. I have no idea how to test this.
+	const scaleFactor = -deltaYInPx/800
+	const scrollAmount = scaleFactor > 0 ? scaleFactor+1 : 1/(-scaleFactor+1)
+	const oldScale = target.currentScale
+	const newScale = oldScale * scrollAmount
+	
+	//Next, calculate the offset to keep the same point under the mousewheel.
+	const cX = (-target.currentTranslate.x/devicePixelRatio + evt.offsetX) / oldScale
+	const cY = (-target.currentTranslate.y/devicePixelRatio + evt.offsetY) / oldScale
+	const nX = (-target.currentTranslate.x/devicePixelRatio + evt.offsetX) / newScale
+	const nY = (-target.currentTranslate.y/devicePixelRatio + evt.offsetY) / newScale
+	
+	//Finally, apply the results of our calculations.
+	target.currentScale = newScale
+	target.currentTranslate.x += (nX-cX)*newScale*devicePixelRatio
+	target.currentTranslate.y += (nY-cY)*newScale*devicePixelRatio
+	
+	//Also don't scroll.
+	evt.preventDefault()
 }

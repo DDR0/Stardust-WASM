62218edec9a0803466686577afb10dac62cc301a
DDR <d@ddr0.ca>
2022-07-06 21:58:33 -0700

    Patch around Webpack's HMR not handling that it's in a worker.

--- www/worker/logicWorker.mjs
+++ www/worker/logicWorker.mjs
@@ -130,3 +130,12 @@ function processFrame() {
 		Promise.resolve().then(processFrame)
 	}
 }
+
+
+
+//Patch around Webpack's HMR not handling that it's in a worker.
+self.window = {
+	location: {
+		reload: postMessage.bind(self, { type:'reload' }),
+	}
+}

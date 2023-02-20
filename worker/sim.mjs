4732b2b8136601599c37599f1553c5ca8fdf7644
DDR <d@ddr0.ca>
2023-02-20 01:07:38 -0800

    Create WASM memory to back the world instead of an array buffer.

--- worker/sim.mjs
+++ worker/sim.mjs
@@ -1,13 +1,15 @@
-const importObject = { imports: { imported_func: (arg) => console.log(arg) } };
-
 const wasmSource = fetch("sim.wasm")
-console.log('idle')
-
-addEventListener("message", ({data}) => {
-	if (data[0] !== "start") throw new Error(`Unknown event '${data[0]}' sent to worker.`)
+addEventListener("message", ({data: [event, workerID, world]}) => {
+	if (event !== "start") throw new Error(`Unknown event '${event}' sent to worker.`)
 	console.log('loading')
-	WebAssembly.compileStreaming(wasmSource, {
-		js: { workerID: new WebAssembly.Global({ value: "i32" }, data[1]) }
+	WebAssembly.instantiateStreaming(wasmSource, {
+		js: { 
+			workerID: new WebAssembly.Global({ value: 'i32' }, workerID), //Can't be passed via message.
+			world,
+		},
+		imports: { 
+			imported_func: (arg) => console.log(arg)
+		},
 	}).then(wasm=>{
 		console.log('run here', wasm)
 	})

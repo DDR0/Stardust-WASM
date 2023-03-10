2a1e1436e2ceb08a28ddee9c14dc0b178b2a3b1f
DDR <d@ddr0.ca>
2023-03-09 20:45:27 -0800

    Added some nice comments explaining what's going on.

--- main.mjs
+++ main.mjs
@@ -1,9 +1,11 @@
+//Must be a shared WebAssembly.Memory.
 const memory = new WebAssembly.Memory({
 	initial: Math.ceil(1),
 	maximum: Math.ceil(1),
 	shared: true,
 })
 
+//Object must have two typed arrays backed by the same buffer.
 const world = {
 	a: new Int32Array(memory.buffer, 0, 0),
 	b: new Int32Array(memory.buffer, 0, 0),
@@ -11,7 +13,8 @@ const world = {
 
 const worker = new Worker('sim.mjs', {type:'module'})
 
-worker.postMessage([world, memory])
-worker.postMessage([memory, world])
-worker.postMessage([world, memory])
-worker.postMessage([memory, world])
+//Object must be posted after Memory
+worker.postMessage([world, memory]) //works
+worker.postMessage([memory, world]) //doesn't work
+worker.postMessage([world, memory]) //still works
+worker.postMessage([memory, world]) //still doesn't
